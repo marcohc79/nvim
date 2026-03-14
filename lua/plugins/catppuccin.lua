5b13c06111d0466a4f683f1ac80ddbab29b89fe1
Marco Huamani <mcaballero79@pm.me>
2026-03-13 21:03:04 -0300

    update theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,8 @@
-return { 
-	"catppuccin/nvim", 
-	name = "catppuccin", 
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	config = function() 
+	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
 			background = { -- :h background
@@ -12,19 +12,19 @@ return {
 			transparent_background = false, -- disables setting the background color.
 			float = {
 				transparent = false, -- enable transparent floating windows
-				solid = false, -- use solid styling for floating windows, see |winborder|
+				solid = false,     -- use solid styling for floating windows, see |winborder|
 			},
 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			term_colors = false,   -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
-				enabled = false, -- dims the background color of inactive window
+				enabled = false,   -- dims the background color of inactive window
 				shade = "dark",
 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
 			},
-			no_italic = false, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+			no_italic = false,     -- Force no italic
+			no_bold = false,       -- Force no bold
+			no_underline = false,  -- Force no underline
+			styles = {             -- Handles the styles of general hi groups (see `:h highlight-args`):
 				comments = { "italic" }, -- Change the style of comments
 				conditionals = { "italic" },
 				loops = {},
